c4352b4fbf7a3c8dd83f4b3a841620c7f36e6c67
choijisul <choisul67@gmail.com>
2024-08-27 22:08:03 +0900

    chap9.1 예제 작성

---
+++ js_study/javascript/chap9(9.1).js
@@ -0,0 +1,19 @@
+var num1 = 10 + 20;
+console.log(num1);
+
+let num2 = 10 + 20;
+// let num2 = 30;  //에러O
+var num3 = 10 + 20;
+var num3 = 50;  //에러x
+console.log(num3);  //50
+
+// console.log(num5);  //에러o
+let num5 = 10;
+console.log(num4);  //undefind
+var num4 = 10;
+
+const num6 = 10;
+// num6 = 30;  //에러o
+// const num7;
+// num7 = 20;  //에러o
+console.log(num6);
