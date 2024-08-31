3294a5712a9285cc50731b62624c978c70efb536
choijisul <choisul67@gmail.com>
2024-08-31 21:09:35 +0900

    chap12.2 예제 작성

---
+++ js_study/javascript/chap12(12.2).js
@@ -0,0 +1,20 @@
+// const el1 = document.firstChild;
+// const el2 = document.firstElementChild;
+
+// const el3 = document.childNodes[1].firstElementChild.firstElementChild.nextElementSibling;
+
+// const el4 = document.getElementById("title");
+// console.log(el4);
+// const classEl1 = document.getElementsByClassName("text");
+// console.log(classEl1);
+// console.log(classEl1[0]);
+// console.log(classEl1[1]);
+// const tagEls = document.getElementsByName("p");
+// console.log(tagEls);
+// console.log(tagEls[0]);
+// console.log(tagEls[1]);
+
+const el5 = document.querySelector(".box-1");
+console.log(el5);
+const el6 = document.querySelectorAll(".box-1 .text");
+console.log(el6);
